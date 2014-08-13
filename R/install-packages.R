29ed5f46dd079393432a97d2709d301e36124a3d
Oliver Meyn <oliver@mineallmeyn.com>
2014-08-13 11:06:37 +0200

    one time packages install script

---
+++ R/install-packages.R
@@ -0,0 +1,3 @@
+install.packages("reshape2", repos="http://cran.rstudio.com/")
+install.packages("plyr", repos="http://cran.rstudio.com/")
+install.packages("ggplot2", repos="http://cran.rstudio.com/")
