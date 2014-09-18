eda8e6b0cf3b0fc88e0cbd283e654f7ad620520d
Oliver Meyn <oliver@mineallmeyn.com>
2014-09-18 15:19:30 +0200

    adds needed hbase jars before running snapshot creation

--- hive/import/hbase/create_hbase_snapshot.q
+++ hive/import/hbase/create_hbase_snapshot.q
@@ -1,3 +1,6 @@
 -- this will only work if the hbase schema hasn't changed!
+ADD JAR ${hiveconf:hivehbase};
+ADD JAR ${hiveconf:hbase};
+
 DROP TABLE IF EXISTS ${hiveconf:snapshotdb}.raw_${hiveconf:snapshot};
 CREATE TABLE ${hiveconf:snapshotdb}.raw_${hiveconf:snapshot} STORED AS RCFILE as select gbifid AS id, datasetkey AS dataset_id, publishingorgkey AS publisher_id, publishingcountry AS publisher_country, `v_.*` from ${hiveconf:sourcedb}.${hiveconf:sourcetable};
--- hive/import/hbase/create_new_snapshot.sh
+++ hive/import/hbase/create_new_snapshot.sh
@@ -3,4 +3,10 @@ if [ "$#" -ne 4 ]; then
     echo "Usage: create_new_snapshot.sh <snapshot hive db e.g. snapshot> <snapshot date e.g. 20140923> <source hive db e.g. prod_b> <source hive table e.g. occurrence_hbase>"
     exit 1
 fi
-hive --hiveconf snapshotdb="$1" --hiveconf snapshot="$2" --hiveconf sourcedb="$3" --hiveconf sourcetable="$4" -f create_hbase_snapshot.q
+# ensure HIVE_HOME is set
+: ${HIVE_HOME:?"Need to set HIVE_HOME env variable to point to Hive's root dir"}
+hhpattern="hive-hbase-handler*jar"
+hivehbase=`find "$HIVE_HOME/lib/" -name "$hhpattern"`
+hbasepattern="hbase-*jar"
+hbase=`find "$HIVE_HOME/lib/" -name "$hbasepattern"`
+hive --hiveconf snapshotdb="$1" --hiveconf snapshot="$2" --hiveconf sourcedb="$3" --hiveconf sourcetable="$4" --hiveconf hivehbase="$hivehbase" --hiveconf hbase="$hbase" -f create_hbase_snapshot.q
