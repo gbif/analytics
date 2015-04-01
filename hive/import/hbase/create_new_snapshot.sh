e57dd323550de66b11f9f3755a63240ce4b92369
timrobertson100 <timrobertson100@gmail.com>
2015-04-01 09:45:12 +0200

    Update create_new_snapshot.sh
    Removing requirement to set $HIVE_HOME as we'll run this on Cloudera gateways now, which don't use this convention

--- hive/import/hbase/create_new_snapshot.sh
+++ hive/import/hbase/create_new_snapshot.sh
@@ -3,8 +3,6 @@ if [ "$#" -ne 4 ]; then
     echo "Usage: create_new_snapshot.sh <snapshot hive db e.g. snapshot> <snapshot date e.g. 20140923> <source hive db e.g. prod_b> <source hive table e.g. occurrence_hbase>"
     exit 1
 fi
-# ensure HIVE_HOME is set
-: ${HIVE_HOME:?"Need to set HIVE_HOME env variable to point to Hive's root dir"}
 hhpattern="hive-hbase-handler*jar"
 hivehbase=`find "$HIVE_HOME/lib/" -name "$hhpattern"`
 hbasepattern="hbase-*jar"
