aa68caca53fc7aa5897782f7fab9042b49e77afe
Oliver Meyn <oliver@mineallmeyn.com>
2015-09-08 12:02:16 -0400

    merge jan fix to query

--- R/country-reports/pg3_fig4_country_downloads_per_month.R
+++ R/country-reports/pg3_fig4_country_downloads_per_month.R
@@ -23,7 +23,8 @@ generateCountryDownloadsPlots <- function() {
     JOIN dataset d ON d.publishing_organization_key = o.key
     JOIN dataset_occurrence_download dod ON d.key = dod.dataset_key
     JOIN occurrence_download od ON od.key = dod.download_key
-    WHERE date(od.created) BETWEEN '2014-07-01' AND '2015-06-30'
+    WHERE od.notification_addresses NOT LIKE '%@gbif.org' AND
+    od.status='SUCCEEDED' AND od.created_by != 'nagios' AND o.country = 'US' AND date(od.created) BETWEEN '2014-07-01' AND '2015-06-30'
     GROUP BY 2,3 ORDER BY 2,3"
   ps_con <- dbConnect(RPostgreSQL::PostgreSQL(), user=ps_user, password=ps_password, dbname=ps_databaseName, host=ps_host)
   downloadsDF <- dbGetQuery(ps_con, ps_sql)
