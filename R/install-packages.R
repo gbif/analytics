3712c88ba6f2a044311234f2a4fa7fbf5451f051
timrobertson100 <timrobertson100@gmail.com>
2016-10-10 16:05:04 +0200

    Adds fix to ensure that the correct GGPlot2 is brought in.  Without this, modern versions will have incorrect stacking order on charts.

--- R/install-packages.R
+++ R/install-packages.R
@@ -2,7 +2,13 @@ install.packages("reshape2", repos="http://cran.rstudio.com/")
 install.packages("plyr", repos="http://cran.rstudio.com/")
 install.packages("Rcppr", repos="http://cran.rstudio.com/")
 install.packages("dplyr", repos="http://cran.rstudio.com/")
-install.packages("ggplot2", repos="http://cran.rstudio.com/")
+
+# !IMPORTANT! Later versions of GGPlot removed the sorting method which we rely on
+# proto is not brought in, so is needed
+#install.packages("ggplot2", repos="http://cran.rstudio.com/") will not work
+install.packages("proto", repos="http://cran.rstudio.com/")
+install.packages("http://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_1.0.1.tar.gz", repos=NULL, type="source")
+
 install.packages("whisker", repos="http://cran.rstudio.com/")
 install.packages("ISOcodes", repos="http://cran.rstudio.com/")
 install.packages("jsonlite", repos="http://cran.r-project.org")
