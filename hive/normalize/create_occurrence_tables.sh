229a2bacf4836278e2831fac2a10309fd5670460
GBIF Developer (Analytics) <informatics+analytics@gbif.org>
2018-10-02 11:28:45 +0200

    Update for 2018 Q3 snapshot, with new Nexus repo

--- hive/normalize/create_occurrence_tables.sh
+++ hive/normalize/create_occurrence_tables.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Builds the occurrence_<date> tables. create_tmp_interp_tables.sh has to be run before this one
 
-curl -SsLo /tmp/occurrence-hive.jar 'http://repository.gbif.org/service/local/artifact/maven/redirect?r=releases&g=org.gbif.occurrence&a=occurrence-hive&v=RELEASE&c=jar-with-dependencies'
+SONAR_REDIRECT_URL=http://repository.gbif.org/repository/gbif/org/gbif/occurrence/occurrence-hive
+VERSION=$(xmllint --xpath "string(//release)" <(curl -s "${SONAR_REDIRECT_URL}/maven-metadata.xml"))
+FILENAME=occurrence-hive-${VERSION}-jar-with-dependencies.jar
+SONAR_DOWNLOAD_URL=${SONAR_REDIRECT_URL}/${VERSION}/${FILENAME}
+curl -SsLo /tmp/occurrence-hive.jar "${SONAR_REDIRECT_URL}/${VERSION}/${FILENAME}"
 
 declare -a mysql_snapshots=("20071219" "20080401" "20080627" "20081010" "20081217" "20090406" "20090617" "20090925" "20091216" "20100401" "20100726" "20101117" "20110221" "20110610" "20110905" "20120118" "20120326" "20120713" "20121031" "20121211" "20130220" "20130521" "20130709" "20130910")
 declare -a hbase_v1_snapshots=("20131220" "20140328")
 declare -a hbase_v2_snapshots=("20140908" "20150119" "20150409")
-declare -a hbase_v3_snapshots=("20150703" "20151001" "20160104" "20160405" "20160704" "20161007" "20161227" "20170412" "20170724" "20171012" "20171222" "20180409" "20180711")
+declare -a hbase_v3_snapshots=("20150703" "20151001" "20160104" "20160405" "20160704" "20161007" "20161227" "20170412" "20170724" "20171012" "20171222" "20180409" "20180711" "20180928")
 
 #echo 'creating mysql snapshots'
 for snapshot in "${mysql_snapshots[@]}"
--- hive/normalize/create_tmp_interp_tables.sh
+++ hive/normalize/create_tmp_interp_tables.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # builds intermediate interp tables (geo and taxonomy). NOTE: make sure the epsg-hsql jar is the right version for the latest release of occurrence-hive!
 
-curl -SsLo /tmp/occurrence-hive.jar 'http://repository.gbif.org/service/local/artifact/maven/redirect?r=releases&g=org.gbif.occurrence&a=occurrence-hive&v=RELEASE&c=jar-with-dependencies'
+SONAR_REDIRECT_URL=http://repository.gbif.org/repository/gbif/org/gbif/occurrence/occurrence-hive
+VERSION=$(xmllint --xpath "string(//release)" <(curl -s "${SONAR_REDIRECT_URL}/maven-metadata.xml"))
+FILENAME=occurrence-hive-${VERSION}-jar-with-dependencies.jar
+SONAR_DOWNLOAD_URL=${SONAR_REDIRECT_URL}/${VERSION}/${FILENAME}
+curl -SsLo /tmp/occurrence-hive.jar "${SONAR_REDIRECT_URL}/${VERSION}/${FILENAME}"
+
 curl -SsLo /tmp/gt-epsg-hsql.jar 'http://download.osgeo.org/webdav/geotools/org/geotools/gt-epsg-hsql/12.1/gt-epsg-hsql-12.1.jar'
 hive --hiveconf occjar=/tmp/occurrence-hive.jar --hiveconf props=hive/normalize/occurrence-processor.properties --hiveconf epsgjar=/tmp/gt-epsg-hsql.jar --hiveconf api=http://api.gbif.org/v1 --hiveconf mapcount=100 -f hive/normalize/interp_geo.q
 hive --hiveconf occjar=/tmp/occurrence-hive.jar --hiveconf props=hive/normalize/occurrence-processor.properties --hiveconf mapcount=100 --hiveconf api=http://api.gbif.org/v1 -f hive/normalize/interp_taxonomy.q
