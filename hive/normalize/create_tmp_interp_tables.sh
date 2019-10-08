728518298055103177034b4af3dfa38767446814
Matthew Blissett <mblissett@gbif.org>
2019-10-08 15:46:48 +0200

    Updated Geotools library version.

--- hive/normalize/create_tmp_interp_tables.sh
+++ hive/normalize/create_tmp_interp_tables.sh
@@ -7,6 +7,6 @@ FILENAME=occurrence-hive-${VERSION}-jar-with-dependencies.jar
 SONAR_DOWNLOAD_URL=${SONAR_REDIRECT_URL}/${VERSION}/${FILENAME}
 curl -SsLo /tmp/occurrence-hive.jar "${SONAR_REDIRECT_URL}/${VERSION}/${FILENAME}"
 
-curl -SsLo /tmp/gt-epsg-hsql.jar 'http://download.osgeo.org/webdav/geotools/org/geotools/gt-epsg-hsql/12.1/gt-epsg-hsql-12.1.jar'
+curl -SsLo /tmp/gt-epsg-hsql.jar 'http://download.osgeo.org/webdav/geotools/org/geotools/gt-epsg-hsql/20.5/gt-epsg-hsql-20.5.jar'
 hive --hiveconf occjar=/tmp/occurrence-hive.jar --hiveconf props=hive/normalize/occurrence-processor.properties --hiveconf epsgjar=/tmp/gt-epsg-hsql.jar --hiveconf api=http://api.gbif.org/v1 --hiveconf mapcount=100 -f hive/normalize/interp_geo.q
 hive --hiveconf occjar=/tmp/occurrence-hive.jar --hiveconf props=hive/normalize/occurrence-processor.properties --hiveconf mapcount=100 --hiveconf api=http://api.gbif.org/v1 -f hive/normalize/interp_taxonomy.q
