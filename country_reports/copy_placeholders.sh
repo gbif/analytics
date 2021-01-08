2f444a02e083480da0ea12f6548766f4369035e5
Matthew Blissett <mblissett@gbif.org>
2021-01-08 22:19:56 +0000

    Create placeholder CSVs for non-publishing area (AX).

--- country_reports/copy_placeholders.sh
+++ country_reports/copy_placeholders.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -e
-# Copy the placeholder image into place for every missing chart, for each of about/figure and publishedBy/figure
+# Copy the placeholder CSV and/or image into place for every missing CSV/chart, for each of about/figure and publishedBy/figure
+
+set -o noclobber
 
 # full path to dir of this script
 workingdir=`dirname $0`
 # report path prefix
 pathprefix="report"
 
-# Denmark will have all the figures:
+# Denmark will have all the CSVs:
 cd $workingdir/../$pathprefix/country/DK
 declare -a filelist
-filelist=(about/figure/*.png about/figure/*.svg publishedBy/figure/*.png publishedBy/figure/*.svg)
+csvlist=(about/csv/*.csv publishedBy/csv/*.csv)
+imagelist=(about/figure/*.png about/figure/*.svg publishedBy/figure/*.png publishedBy/figure/*.svg)
 cd -
 
 # for every country in report/
@@ -18,9 +21,17 @@ for country in $countries; do
 	countrycode=`echo $country | rev | cut -d \/ -f 1 | rev`
 	# echo "$countrycode"
 	path=$pathprefix/country/$countrycode
-	mkdir -p $path/about/figure $path/publishedBy/figure
+	mkdir -p $path/about/csv $path/publishedBy/csv $path/about/figure $path/publishedBy/figure
 
-	for filename in "${filelist[@]}"; do
+	for filename in "${csvlist[@]}"; do
+		# echo "Checking $country/$filename"
+		if [ ! -f $country/$filename ]; then
+			# echo "Copying into $country"
+			head -n 1 "$workingdir/../$pathprefix/country/DK/$filename" > $country/$filename
+		fi
+	done
+
+	for filename in "${imagelist[@]}"; do
 		# echo "Checking $country/$filename"
 		if [ ! -f $country/$filename ]; then
 			# echo "Copying into $country"
