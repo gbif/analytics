6dd9844772a2c5f873cf8e1e31ca6c1800cc192a
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2023-12-04 23:04:21 +0100

    trino scripts

--- trino/rscript.sh
+++ trino/rscript.sh
@@ -3,44 +3,44 @@ PyScript="docker run --rm -it -v $PWD:/analytics/ -v $(realpath hadoop/):/analyt
 # Set the permissions correctly afterwards
 RscriptChown="docker run --rm -it -v $PWD:/analytics/ docker.gbif.org/analytics-figures chown --recursive --from root:root --reference build.sh report"
 
-log 'Preparing the CSVs'
-log 'R script occ_kingdomBasisOfRecord.R'
+echo 'Preparing the CSVs'
+echo 'R script occ_kingdomBasisOfRecord.R'
 $Rscript R/csv/occ_kingdomBasisOfRecord.R
-log 'R script occ_dayCollected.R'
+echo 'R script occ_dayCollected.R'
 $Rscript R/csv/occ_dayCollected.R
-log 'R script occ_yearCollected.R'
+echo 'R script occ_yearCollected.R'
 $Rscript R/csv/occ_yearCollected.R
-log 'R script occ_complete.R'
+echo 'R script occ_complete.R'
 $Rscript R/csv/occ_complete.R
-log 'R script occ_repatriation.R'
+echo 'R script occ_repatriation.R'
 $Rscript R/csv/occ_repatriation.R
-log 'R script occ.R'
+echo 'R script occ.R'
 $Rscript R/csv/occ.R
-log 'R script occ_cells.R'
+echo 'R script occ_cells.R'
 $Rscript R/csv/occ_cells.R
-log 'Python script occ_density.py'
+echo 'Python script occ_density.py'
 $PyScript R/geotiff/occ_density.py
 
-log 'R script spe_kingdom.R'
+echo 'R script spe_kingdom.R'
 $Rscript R/csv/spe_kingdom.R
-log 'R script spe_dayCollected.R'
+echo 'R script spe_dayCollected.R'
 $Rscript R/csv/spe_dayCollected.R
-log 'R script spe_yearCollected.R'
+echo 'R script spe_yearCollected.R'
 $Rscript R/csv/spe_yearCollected.R
-log 'R script spe_repatriation.R'
+echo 'R script spe_repatriation.R'
 $Rscript R/csv/spe_repatriation.R
-log 'R script spe.R'
+echo 'R script spe.R'
 $Rscript R/csv/spe.R
 $RscriptChown
 
-log '############################'
-log 'PROCESS CSVS STAGE COMPLETED'
-log '############################'
+echo '############################'
+echo 'PROCESS CSVS STAGE COMPLETED'
+echo '############################'
 
-log 'Generating the figures'
+echo 'Generating the figures'
 $Rscript R/report.R
 $RscriptChown
 
-log 'Copying placeholders'
+echo 'Copying placeholders'
 country_reports/copy_placeholders.sh
 
