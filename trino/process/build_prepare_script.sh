1a13218c8d5e355729434b5d1943137e1ebc2935
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2023-12-11 14:44:01 +0100

    (WIP) analytics scripts using trino

--- trino/process/build_prepare_script.sh
+++ trino/process/build_prepare_script.sh
@@ -25,7 +25,7 @@ SELECT * FROM
 for snapshot in "${snapshots[@]}"
 do
   tmptable=${snapshot:0:4}-${snapshot:4:2}-${snapshot:6:2}
-  echo "SELECT '${tmptable}'"' AS snapshot, CAST(id AS BIGINT), CAST(dataset_id AS varchar) AS dataset_id,
+  echo "SELECT '${tmptable}'"' AS snapshot, CAST(id AS BIGINT) AS id, CAST(dataset_id AS varchar) AS dataset_id,
         CAST(publisher_id AS varchar) AS publisher_id, kingdom, phylum, class_rank, order_rank, family, genus, species,
         scientific_name, kingdom_id, phylum_id, class_id, order_id, family_id, genus_id, species_id, taxon_id, basis_of_record,
         latitude, longitude, country, day, month, year, publisher_country, gbif_region, publisher_gbif_region
