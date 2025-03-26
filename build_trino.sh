d18e3c10f2bf3af8d1483514cfdf603bd3f69f14
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2025-03-26 14:20:42 +0100

    Rscript adapted to container

--- build_trino.sh
+++ build_trino.sh
@@ -120,33 +120,33 @@ if [ $processCsvs == "true" ]; then
 
   log 'Preparing the CSVs'
   log 'R script occ_kingdomBasisOfRecord.R'
-  $Rscript R/csv/occ_kingdomBasisOfRecord.R
+  Rscript R/csv/occ_kingdomBasisOfRecord.R
   log 'R script occ_dayCollected.R'
-  $Rscript R/csv/occ_dayCollected.R
+  Rscript R/csv/occ_dayCollected.R
   log 'R script occ_yearCollected.R'
-  $Rscript R/csv/occ_yearCollected.R
+  Rscript R/csv/occ_yearCollected.R
   log 'R script occ_complete.R'
-  $Rscript R/csv/occ_complete.R
+  Rscript R/csv/occ_complete.R
   log 'R script occ_repatriation.R'
-  $Rscript R/csv/occ_repatriation.R
+  Rscript R/csv/occ_repatriation.R
   log 'R script occ.R'
-  $Rscript R/csv/occ.R
+  Rscript R/csv/occ.R
   log 'R script occ_cells.R'
-  $Rscript R/csv/occ_cells.R
+  Rscript R/csv/occ_cells.R
   log 'Python script occ_density.py'
   $PyScript R/geotiff/occ_density.py
 
   log 'R script spe_kingdom.R'
-  $Rscript R/csv/spe_kingdom.R
+  Rscript R/csv/spe_kingdom.R
   log 'R script spe_dayCollected.R'
-  $Rscript R/csv/spe_dayCollected.R
+  Rscript R/csv/spe_dayCollected.R
   log 'R script spe_yearCollected.R'
-  $Rscript R/csv/spe_yearCollected.R
+  Rscript R/csv/spe_yearCollected.R
   log 'R script spe_repatriation.R'
-  $Rscript R/csv/spe_repatriation.R
+  Rscript R/csv/spe_repatriation.R
   log 'R script spe.R'
-  $Rscript R/csv/spe.R
-  $RscriptChown
+  Rscript R/csv/spe.R
+  RscriptChown
 
   log '############################'
   log 'PROCESS CSVS STAGE COMPLETED'
@@ -157,8 +157,8 @@ fi
 
 if [ $makeFigures == "true" ]; then
   log 'Generating the figures'
-  $Rscript R/report.R
-  $RscriptChown
+  Rscript R/report.R
+  RscriptChown
 
   log 'Copying placeholders'
   country_reports/copy_placeholders.sh
